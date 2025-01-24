c5f4d1689cf3a89be0e06df531213967be9a7aa7
CodeByAnkita <ankitacode11@gmail.com>
2025-01-24 18:33:38 +0700

    added common partnercard

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-// import Link from 'next/link';
+import Link from 'next/link';
 import Image from "next/image";
 import Aboutfirm from "@/public/about/aboutfirm.jpg";
 import ScrollingLogos from "../components/ScrollingLogos";
